c10d64941965a802d516cea9921af4247625924e
joeaaa <zdnf_lgq@163.com>
2023-05-30 18:40:21 +0800

    feat: new await

--- type-challenges/00189-easy-awaited/template.ts
+++ type-challenges/00189-easy-awaited/template.ts
@@ -1,13 +1,20 @@
-type Thenable<T> = {
-  then: (onfulfilled: (arg: T) => unknown) => unknown;
-};
-type MyAwaited<T extends Thenable<any> | Promise<any>> = T extends Promise<
-  infer X
->
-  ? X extends Promise<any>
-    ? MyAwaited<X>
-    : X
-  : T extends Thenable<infer Z>
-  ? Z
-  : false;
+// type Thenable<T> = {
+//   then: (onfulfilled: (arg: T) => unknown) => unknown;
+// };
+// type MyAwaited<T extends Thenable<any> | Promise<any>> = T extends Promise<
+//   infer X
+// >
+//   ? X extends Promise<any>
+//     ? MyAwaited<X>
+//     : X
+//   : T extends Thenable<infer Z>
+//   ? Z
+//   : false;
 // infer U infer是ts推断api U用来接收
+
+type MyAwaited<T extends PromiseLike<any | PromiseLike<any>>> =
+  T extends PromiseLike<infer X>
+    ? X extends PromiseLike<any>
+      ? MyAwaited<X>
+      : X
+    : never;
